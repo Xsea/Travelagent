d93c1e43adf92b5a5a7a9a4cca8a167cb87e7b5c
DanielToews <daniel.toews@codecentric.de>
2025-01-09 06:57:40 +0100

    tools added

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+import json
+
 from openai import OpenAI
+
+from tools import calculate_hotel_cost
+from tools_description import calculate_hotel_cost_tool
 
 client = OpenAI()
 
 assistantMessage = "How can I help you?"
-# the chat history - you need to fill it
-messages = []
+messages = [{"role": "system",
+             "content": """You are a traveling agent, responsible for helping people planning their vacations.
+             Users will come to you searching for advice on traveling locations. Please answer, so that they find a nice 
+             location to spend their holidays
+             """}]
+
+tools = [calculate_hotel_cost_tool]
 while True:
-    # accepts user input
     userRequest = str(input(assistantMessage + "\n"))
-    # exit
     if userRequest == "thanks":
         break
 
-    # this sends the request to the llm - answer you can find in the completionRequest
-    completionResponse = client.chat.completions.create(
+    messages.append({"role": "user", "content": userRequest})
+    # read user input and devise a plan on how to solve it
+    completionRequest = client.chat.completions.create(
         model="gpt-4o-mini",
-
+        tools=tools,
+        messages= messages
         )
 
-    # things that need to be done: write a system
-    # fill the messages array so that the complete chat history is there and give it to the llm
-    # update the assistant message, so that the user can see what the LLM answered
+    if completionRequest.choices[0].finish_reason == "tool_calls":
+        tool_call = completionRequest.choices[0].message.tool_calls[0]
+        arguments = json.loads(tool_call.function.arguments)
+        if tool_call.function.name == "calculate_hotel_cost":
+            cost = calculate_hotel_cost(arguments["start_date"], arguments["end_date"])
+            messages.append({"role": "system",
+             "content": """The cost of the stay with arrival date {} and departure date {} will be {}""".format(
+                 arguments["start_date"], arguments["end_date"], cost
+                 )})
+            completionRequest = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages= messages
+            )
+
+    llm_answer = completionRequest.choices[0].message
+    messages.append(llm_answer)
+    assistantMessage = llm_answer.content
---
+++ tools.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def calculate_duration(start_date_string, end_date_string):
+    start_date = datetime.strptime(start_date_string, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_string, "%Y-%m-%d")
+    delta = end_date-start_date
+    return delta.days
+
+def calculate_hotel_cost(start_date, end_date):
+    number_of_days = calculate_duration(start_date, end_date)
+    return number_of_days * 100
