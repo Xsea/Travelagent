dd5aad6dda1aec2ec9c3c2f06c5b83388f620902
DanielToews <daniel.toews@codecentric.de>
2025-03-26 19:50:39 +0100

    rag stuff

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from openai import OpenAI
+import psycopg2
+
+db_params = {
+    'dbname': 'vectorDB',
+    'user': 'user',
+    'password': 'pass',
+    'host': 'localhost',
+    'port': 5432
+}
+connection = psycopg2.connect(**db_params)
+cursor = connection.cursor()
 
 client = OpenAI()
 
