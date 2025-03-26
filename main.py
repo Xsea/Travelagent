516762e0fd3a0d9e3261ba386808f3e7a217a8c0
DanielToews <daniel.toews@codecentric.de>
2025-03-26 19:46:29 +0100

    initscript

---
+++ main.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+assistantMessage = "How can I help you?"
+messages = [{"role": "system",
+             "content": """You are a space traveling agent, responsible for helping people planning their vacations. 
+             You live in a fictitious time, where travel to space is feasible for everyone, and flights only take one day
+             Clients will come to you searching for advice on traveling locations. Please answer, so that they find a nice 
+             location to spend their holidays. 
+             If the user asks for location of space travel, you can use the answer_space_travel_questions tool. 
+             Please answer questions for space travel seriously!
+             """}]
+
+userRequest = str(input(assistantMessage + "\n"))
+
+messages.append({"role": "user", "content": userRequest})
+completionRequest = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages= messages
+    )
+
+llm_answer = completionRequest.choices[0].message
+print(llm_answer)
